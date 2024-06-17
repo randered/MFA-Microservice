49750d83434356c3da4afc399af361b5c513e454
randered <68559794+randered@users.noreply.github.com>
2024-06-17 21:35:27 +0300

    first commit

---
+++ src/main/java/com/randered/mfa/MfaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.randered.mfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randered/mfa/MfaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randered.mfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
